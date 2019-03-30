395faf4c398b4e3235c1734986c67d35cad194ea
Hauser <hauser@users.noreply.github.com>
2019-03-30 13:42:17 +0100

    Finish project

--- autoc.js
+++ autoc.js
@@ -1,31 +1,38 @@
-const autoCorrelation = (x, normalize = false, scaling = false) => {
+const crossCorrelation = (x, y, normalize = false, scaling = false) => {
     /*
               N-1
-        c[i] = Σ x[n] * x[n-i]
+        c[i] = Σ x[n] * y[n-i]
               n=0
     */
 
     let c = [];
     const N = x.length;
-    for (let i = 0; i < N; i++) {
+    for (let i = -(y.length - 1); i < N; i++) {
         let sum = 0;
         for (let n = 0; n < N; ++n) {
-            if (!isNaN(x[n - i])) {
-                sum += x[n] * x[n - i];
+            if (!isNaN(y[n - i])) {
+                sum += x[n] * y[n - i];
             }
         }
         c.push(sum);
     }
 
     if (normalize || scaling) {
-        const max = normalize ? Math.max(...c) : 1;
-        const scale = scaling ? Math.max(...x) : 1;
+        const max = normalize ? Math.max(...c.map(n => Math.abs(n))) : 1;
+        const scale = scaling ? Math.max(...x.map(n => Math.abs(n))) : 1;
         c = c.map(x => x / max * scale);
     }
 
     return c;
 };
 
+const autoCorrelation = (x, normalize = false, scaling = false) => {
+    return crossCorrelation(x, x, normalize, scaling);
+};
+
 if (typeof module !== 'undefined' && module.exports) {
-    module.exports = autoCorrelation;
+    module.exports = {
+        crossCorrelation,
+        autoCorrelation,
+    };
 }
--- autoc.test.js
+++ autoc.test.js
@@ -1,25 +1,48 @@
-const autoCorrelation = require('./autoc');
+const { autoCorrelation, crossCorrelation } = require('./autoc');
 
-test('autoCorrelation(x)', () => {
-    let x = [1, 2, 3, 4];
+test('autoCorrelation([0, 2, 3, -4])', () => {
+    let x = [0, 2, 3, -4];
     let c = autoCorrelation(x);
-    expect(c).toEqual(expect.arrayContaining([30, 20, 11, 4]));
+    expect(c).toEqual(expect.arrayContaining([0, -8, -6, 29, -6, -8, 0]));
 });
 
-test('autoCorrelation(x, true)', () => {
-    let x = [1, 2, 3, 4];
+test('autoCorrelation([0, 2, 3, -4], true)', () => {
+    let x = [0, 2, 3, -4];
     let c = autoCorrelation(x, true);
-    expect(c).toEqual(expect.arrayContaining([30 / 30, 20 / 30, 11 / 30, 4 / 30]));
+    expect(c).toEqual(expect.arrayContaining([0 / 29, -8 / 29, -6 / 29, 29 / 29, -6 / 29, -8 / 29, 0 / 29]));
 });
 
-test('autoCorrelation(x, true, true)', () => {
-    let x = [1, 2, 3, 4];
+test('autoCorrelation([0, 2, 3, -4], true, true)', () => {
+    let x = [0, 2, 3, -4];
     let c = autoCorrelation(x, true, true);
-    expect(c).toEqual(expect.arrayContaining([30 / 30 * 4, 20 / 30 * 4, 11 / 30 * 4, 4 / 30 * 4]));
+    expect(c).toEqual(expect.arrayContaining([0 / 29 * 4, -8 / 29 * 4, -6 / 29 * 4, 29 / 29 * 4, -6 / 29 * 4, -8 / 29 * 4, 0 / 29 * 4]));
 });
 
-test('autoCorrelation(x, false, true)', () => {
-    let x = [1, 2, 3, 4];
+test('autoCorrelation([0, 2, 3, -4], false, true)', () => {
+    let x = [0, 2, 3, -4];
     let c = autoCorrelation(x, false, true);
-    expect(c).toEqual(expect.arrayContaining([30 * 4, 20 * 4, 11 * 4, 4 * 4]));
+    expect(c).toEqual(expect.arrayContaining([0 * 4, -8 * 4, -6 * 4, 29 * 4, -6 * 4, -8 * 4, 0 * 4]));
+});
+
+test('crossCorrelation([8, 9, 2, 3], [4, 3, 6])', () => {
+    let x = [8, 9, 2, 3];
+    let y = [4, 3, 6];
+    let c = crossCorrelation(x, y);
+    expect(c).toEqual(expect.arrayContaining([48, 78, 71, 60, 17, 12]));
+});
+
+test('crossCorrelation([0, 1, 2, 0, 0], [0, 0, 0, 1, 2])', () => {
+    let x = [0, 1, 2, 0, 0];
+    let y = [0, 0, 0, 1, 2];
+    let c = crossCorrelation(x, y);
+    expect(c).toEqual(expect.arrayContaining([0, 2, 5, 2, 0, 0, 0, 0, 0]));
+});
+
+test('crossCorrelation([4, 3, 6], [4, 3, 6])', () => {
+    let x = [4, 3, 6];
+    let y = [4, 3, 6];
+    let c = crossCorrelation(x, y);
+    let ac = autoCorrelation(x);
+    expect(c).toEqual(expect.arrayContaining([24, 30, 61, 30, 24]));
+    expect(c).toEqual(expect.arrayContaining(ac));
 });
